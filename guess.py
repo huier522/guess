3369c6795c46f6fa19af8be9a83f6c77b44b7116
huier <andychen522.ambit@gmail.com>
2021-05-11 00:33:30 +0800

    add counter function

--- guess.py
+++ guess.py
@@ -1,12 +1,14 @@
 import random
 ans = random.randint(1, 100)
+times = 0
 
 while True:
     guess = int(input('1~100 請猜一個數字：'))
+    times = times + 1
     if guess > ans:
         print('你猜的數字比答案大')
     elif guess < ans:
         print('你猜的數字比答案小')
     elif guess == ans:
-        print(f'恭喜答對了，答案是{ans}')
+        print(f'恭喜答對了，答案是{ans}，總共猜了{times}次！')
         break
