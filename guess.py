072fccf655e77e9efb1021352931a22c4cfcdb1a
huier <andychen522.ambit@gmail.com>
2021-05-10 22:47:36 +0800

    add function for user can guess answer

--- guess.py
+++ guess.py
@@ -3,3 +3,10 @@ ans = random.randint(1, 100)
 
 while True:
     guess = int(input('1~100 請猜一個數字：'))
+    if guess > ans:
+        print('你猜的數字比答案大')
+    elif guess < ans:
+        print('你猜的數字比答案小')
+    elif guess == ans:
+        print(f'恭喜答對了，答案是{ans}')
+        break
