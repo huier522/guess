4ef8c2eb356b02cb37542c40f718fb6907323e09
huier <andychen522.ambit@gmail.com>
2021-05-12 00:07:55 +0800

    add function for use define the range

--- guess.py
+++ guess.py
@@ -1,9 +1,11 @@
 import random
-ans = random.randint(1, 100)
+start = int(input('請輸入起始整數：'))
+end = int(input('請輸入終點整數：'))
+ans = random.randint(start, end)
 times = 0
 
 while True:
-    guess = int(input('1~100 請猜一個數字：'))
+    guess = int(input('請猜一個數字：'))
     times = times + 1
     if guess > ans:
         print('你猜的數字比答案大')
